2342d961f468df0d2c0b9a1b34fb22122c40dcc8
OmastewinF <omaste-win.foster@umconnect.umt.edu>
2023-01-23 14:07:33 -0700

    week 1

---
+++ Week1/sketch.js
@@ -0,0 +1,32 @@
+function setup() {
+  createCanvas(800, 400);
+}
+
+function draw() {
+  background('magenta');
+    
+  fill('black')
+rect(300,250,10,30)
+  
+  fill('red');
+  circle(310,300,70);
+  
+fill('black');
+  rect (395,250,10,30);
+  
+  fill ('red');
+  circle(400,300,70);
+  
+fill('black')
+rect(210,180,10,30);
+  
+  fill('yellow');
+  rect(200,250,50,70);
+  fill('yellow');
+  rect(200,200,40,60)
+  fill('yellow');
+  rect(200,300,65,50);
+ 
+  
+  
+}
